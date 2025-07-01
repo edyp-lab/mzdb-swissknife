bc9eed5814d4324817ea07c771a6a7b8037e5c5b
vd225637 <veronique.dupierris@cea.fr>
2025-07-01 11:11:36 +0200

    Take into account mgfBoost Command parameter in MgfBoostConfigTemplate and panel param conversion: swMaxPrecursorsCount & swIntensityThreshold removed and rankThreshold renamed to takeThreshold

--- src/main/java/fr/profi/mzknife/mzdb/MgfBoostConfigTemplate.java
+++ src/main/java/fr/profi/mzknife/mzdb/MgfBoostConfigTemplate.java
@@ -4,27 +4,23 @@ import fr.profi.mzknife.CommandArguments;
 
 public enum MgfBoostConfigTemplate {
 
-  DISCOVERY( false,true ,2 , 0.0f , CommandArguments.ScanSelectorMode.ALL , 0.125 , 2),
-  HISTONE( true,true ,1 , 0.2f , CommandArguments.ScanSelectorMode.SAME_CYCLE , 0.125 , 5),
-  XLINK( false,true ,2 , 0.0f , CommandArguments.ScanSelectorMode.SAME_CYCLE, 0.125 , 2);
+  DISCOVERY( false,true , CommandArguments.ScanSelectorMode.ALL , 0.125 , 2),
+  HISTONE( true,true , CommandArguments.ScanSelectorMode.SAME_CYCLE , 0.125 , 5),
+  XLINK( false,true , CommandArguments.ScanSelectorMode.SAME_CYCLE, 0.125 , 2);
 
   final boolean useHeader;
   final boolean useSelectionWindow;
-  final int swMaxPrecursorsCount;
-  final float swIntensityThreshold;
   final CommandArguments.ScanSelectorMode scanSelector;
   final double pifThreshold;
-  final int rankThreshold;
+  final int takeThreshold;
 
 
-  MgfBoostConfigTemplate(boolean useHeader, boolean useSelectionWindow, int swMaxPrecursorsCount, float swIntensityThreshold, CommandArguments.ScanSelectorMode scanSelector, double pifThreshold, int rankThreshold) {
+  MgfBoostConfigTemplate(boolean useHeader, boolean useSelectionWindow, CommandArguments.ScanSelectorMode scanSelector, double pifThreshold, int takeThreshold) {
     this.useHeader = useHeader;
     this.useSelectionWindow = useSelectionWindow;
-    this.swMaxPrecursorsCount = swMaxPrecursorsCount;
-    this.swIntensityThreshold = swIntensityThreshold;
     this.scanSelector = scanSelector;
     this.pifThreshold = pifThreshold;
-    this.rankThreshold = rankThreshold;
+    this.takeThreshold = takeThreshold;
   }
 
   public boolean isUseHeader() {
@@ -35,13 +31,6 @@ public enum MgfBoostConfigTemplate {
     return useSelectionWindow;
   }
 
-  public int getSwMaxPrecursorsCount() {
-    return swMaxPrecursorsCount;
-  }
-
-  public float getSwIntensityThreshold() {
-    return swIntensityThreshold;
-  }
 
   public CommandArguments.ScanSelectorMode getScanSelector() {
     return scanSelector;
@@ -51,7 +40,7 @@ public enum MgfBoostConfigTemplate {
     return pifThreshold;
   }
 
-  public int getRankThreshold() {
-    return rankThreshold;
+  public int getTakeThreshold() {
+    return takeThreshold;
   }
 }
